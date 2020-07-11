da06c5d3bd1f5514f5e4a6bc8e0e15aa423b5edd
ArianaGu <1447602704@qq.com>
2020-07-11 07:52:00 -0400

    Load comments from data using JSON

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -20,13 +20,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+import java.util.ArrayList; 
+
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private ArrayList<String> user_comments = new ArrayList<String>();
+  
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello Chaoying!");
+    user_comments.add("Hi, welcome to leave a comment");
+    Gson gson = new Gson();
+    String json = gson.toJson(user_comments);
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
   }
 }
